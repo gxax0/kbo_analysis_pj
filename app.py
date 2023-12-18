02a352cd19f53e9c3d7fdcb586b6f57577cfc53d
개영 <129811867+gxax0@users.noreply.github.com>
2023-12-18 22:07:09 +0900

    app.py

--- app.py
+++ app.py
@@ -8,13 +8,17 @@ Original file is located at
 """
 
 import streamlit as st
+import seaborn as sns
 import pandas as pd
-import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-import plot as pl
+ 
 
 # st.title('')
 st.header('⚾ KBO 한국 프로야구 우승팀 예측 모델 📈')
 st.markdown("""---""")
 data = pd.read_csv('./kbo_merge_data.csv')
+
+# Seaborn correlation plot 생성
+plot = sns.heatmap(df.corr(), annot=True)
+ 
+# 플롯을 Streamlit에 표시
+st.pyplot(plot.get_figure())
