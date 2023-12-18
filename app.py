4e5e4527943139bf1e4fd30ecfdee246524be496
개영 <129811867+gxax0@users.noreply.github.com>
2023-12-18 23:53:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
 from xgboost import XGBRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
-import numpy as np
 import matplotlib.pyplot as plt
 
-# st.title('')
 st.header('⚾ KBO 한국 프로야구 우승팀 예측 모델 📈')
 st.markdown("""---""")
 df = pd.read_csv('./kbo_merge_data.csv')
 
+# seaborn pairplot 생성
+plot = sns.pairplot(df)
+
+# plot을 streamlit에 표시
+st.pyplot(plot)
 
 # 피처 선택
 features = ['ERA+', 'Save', 'WAR', 'Lose']
@@ -30,15 +31,15 @@ model.fit(X_train, y_train)
 
 # 예측
 y_pred = model.predict(X_test)
-# 팀 이름과 예측 승수를 x, y로 설정
-x = team_wins['Team']
-y = team_wins['Win_predicted']
+
+# 팀 이름과 예측 승수 설정
+team_wins = pd.DataFrame({'Team': df['Team'][X_test.index], 'Win_predicted': y_pred})
 
 # 그래프 사이즈 설정
 plt.figure(figsize=(10, 8))
 
 # 바 차트 생성
-plt.barh(x, y, color='skyblue')
+plt.barh(team_wins['Team'], team_wins['Win_predicted'], color='skyblue')
 
 # 그래프 제목 및 x, y 레이블 설정
 plt.title('Team Winning Prediction')
@@ -48,5 +49,5 @@ plt.ylabel('Team')
 # y축의 순서를 높은 순위가 위로 오도록 변경
 plt.gca().invert_yaxis()
 
-# 그래프 보여주기
-plt.show()
+# Streamlit에 Matplotlib 그림 표시
+st.pyplot(plt)
